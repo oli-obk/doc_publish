9fa27ebfeb6e1f1fdbf25d7843dad9d06faa007a
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2016-05-27 10:38:14 +0200

    updated to imperio's new css

--- implementors/rustc/dep_graph/dep_tracking_map/trait.DepTrackingMapConfig.js
+++ implementors/rustc/dep_graph/dep_tracking_map/trait.DepTrackingMapConfig.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["rustc_lint"] = [];implementors["rustc_metadata"] = [];implementors["rustdoc"] = [];
+implementors["rustc_lint"] = [];implementors["rustc_trans"] = [];implementors["rustc_metadata"] = [];implementors["rustdoc"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/rustc_data_structures/snapshot_vec/trait.SnapshotVecDelegate.js
+++ implementors/rustc_data_structures/snapshot_vec/trait.SnapshotVecDelegate.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["rustc_data_structures"] = [];
+implementors["rustc_data_structures"] = [];implementors["rustc_trans"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
